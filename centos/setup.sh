34d645db40862ea6d38f01931fee0c94420c39d8
Jed <lejosnej@ainfosec.com>
2016-01-12 17:12:59 -0500

    centos setup: minor fixes
    Signed-off-by: Jed <lejosnej@ainfosec.com>

--- centos/setup.sh
+++ centos/setup.sh
@@ -5,7 +5,15 @@ passwd -d root
 
 # Install required packages
 rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
-yum -y install rpm-build createrepo which sudo git which wget gcc kernel-devel tar dkms
+yum -y install rpm-build createrepo which sudo git which wget gcc make kernel-devel tar dkms
+
+# Setup symlinks to make dkms happy
+for kernelpath in `ls /usr/src/*`; do
+    kernel=`basename $kernelpath`
+    mkdir -p /lib/modules/${kernel}
+    [ -e /lib/modules/${kernel}/build ] || ln -s ${kernelpath} /lib/modules/${kernel}/build
+    [ -e /lib/modules/${kernel}/source ] || ln -s ${kernelpath} /lib/modules/${kernel}/source
+done
 
 # Add a build user
 adduser build
